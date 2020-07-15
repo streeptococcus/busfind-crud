91c220f93ab3e0f53cfdfa97df98a83ef476223e
streeptococcus <anapalmeidaj1@gmail.com>
2020-07-15 14:21:25 -0300

    Securing files and informations.

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
 import { createPool } from 'mysql2/promise'
+require('dotenv').config()
 
 export async function connect() {
   const connection = await createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '12345',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'busfind',
     connectionLimit: 10,
   })
